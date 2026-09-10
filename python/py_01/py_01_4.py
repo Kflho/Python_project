9736300024c6be4fd6c4b6970c7388e916011ffc
Kflho <kaho1728@qq.com>
2026-09-10 12:41:27 +0800

    .gitignore

---
+++ python/py_01/py_01_4.py
@@ -0,0 +1,10 @@
+num_1=3
+num_2=10
+total=num_1+num_2
+print(num_1)
+
+a=666
+b=a
+print(b)
+a=999
+print(a)
---
+++ python/py_01/py_01_5.py
@@ -0,0 +1,30 @@
+num_1=1
+print(type(num_1))
+
+num_2=1.5
+print(type(num_2))
+
+num_3=True
+num_4=False
+print(True+False)
+print(True+1)
+
+num_5=1+2j
+print(type(num_5))
+
+num_6='sixstar'
+print(type(num_6))
+print(num_6)
+
+name='Kflho'
+print("my name is %s"%name)
+age=22
+print("my year is %d"%age)
+print("my name is %s, my year is %d"%(name,age))
+
+a=1.3
+print("%f"%a)
+
+print("I'm %%"%())
+
+print(f"my name is {name}, i am {age} years old")
