3c5da68afbc79e86ed8cdf11f4bb58ed799f190b
Kflho <kaho1728@qq.com>
2026-09-25 18:07:53 +0800

    1.6

---
+++ python/py_01/py_01_6.py
@@ -0,0 +1,23 @@
+print(1+1-1/1)
+print(type(1/1))
+print(5//2)
+print(5%2)
+print(5**2)
+a=5
+b=2
+c=a+b
+a+=1
+print(a)
+a+=b
+print(a)
+
+# 输入函数
+name=input("输入")
+print(name)
+# 转义字符
+print("a\tb")
+print("a\nb")
+print("a\rb")
+print("a\\b")
+
+
